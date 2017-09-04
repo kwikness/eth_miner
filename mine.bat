02bed5625b13b932434aacd417bdaab744108310
kwikness <kwikness@gmail.com>
2017-09-04 15:57:12 -0400

    init

---
+++ mine.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+ethminer.exe --farm-recheck 200 -G -S us1.ethermine.org:4444 -FS us2.ethermine.org:4444 -O 0xf53d7231bc3b6326a8011b8617c6ac8a0019bc96.thxBr0
